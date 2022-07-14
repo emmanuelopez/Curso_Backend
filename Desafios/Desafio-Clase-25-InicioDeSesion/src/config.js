c8c6d247f8aa9d63eccfe8746d846a5bbf7a3ad2
Emmanuel Lopez <eelopez29@gmail.com>
2022-07-13 23:18:42 -0300

    Agrego desafio de la clase de Global-Child Process

--- Desafios/Desafio-Clase-25-InicioDeSesion/src/config.js
+++ Desafios/Desafio-Clase-25-InicioDeSesion/src/config.js
@@ -2,7 +2,7 @@ export default {
     PORT: process.env.PORT || 8080,
     mongoRemote: {
         client: 'mongodb',
-        cnxStr: 'mongodb+srv://eel:4GDJDZvBBKOK6ZaA@cluster0.zib5u.mongodb.net/miBaseLogin?retryWrites=true&w=majority'
+        cnxStr: 'mongodb+srv://eelop:MOSfet2905@cluster0.lhzzn.mongodb.net/?retryWrites=true&w=majority'
     },
     fileSystem: {
         path: './DB'
